5dbf57c89f250586f7e6bec6db252cd78b7f6d49
Shivnarayan Andhare <107403878+Shivnarayan07@users.noreply.github.com>
2024-09-03 23:43:58 +0530

    Time: 1 ms (73.01%), Space: 41.2 MB (79.82%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,25 +1,22 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        if (strs == null || strs.length == 0) {
-            return "";
-        }
 
-       
-        String prefix = strs[0];
+        StringBuilder ans = new StringBuilder();
+        Arrays.sort(strs);
+        String first = strs[0];
+        String last = strs[strs.length-1];
 
-        
-        for (int i = 1; i < strs.length; i++) {
-            while (strs[i].indexOf(prefix) != 0) {
-               
-                prefix = prefix.substring(0, prefix.length() - 1);
+       for(int i = 0 ; i < Math.min(first.length(), last.length()) ; i++){
 
-               
-                if (prefix.isEmpty()) {
-                    return "";
-                }
+            if(first.charAt(i) != last.charAt(i)){
+                return ans.toString();
+
             }
+
+            ans.append(first.charAt(i));
         }
 
-        return prefix;
+        return ans.toString();
+        
     }
 }
